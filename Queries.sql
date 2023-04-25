378aaa7dbe0d3b6df0720b825f04ae70f71d07ef
Anisa Braun <125165703+anisabmarie@users.noreply.github.com>
2023-04-24 20:49:31 -0600

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,74 @@
+SELECT employees.emp_no, 
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+	
+FROM employees
+LEFT JOIN salaries
+ON employees.emp_no = salaries.emp_no
+ORDER BY emp_no
+
+SELECT * FROM employees
+WHERE DATE_PART('year',hire_date) = 1986
+ORDER BY emp_no;
+
+SELECT dept_manager.dept_no, 
+	departments.dept_name,
+	dept_manager.emp_no,
+	employees.last_name, 
+	employees.first_name	
+FROM dept_manager
+LEFT JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees 
+ON dept_manager.emp_no = employees.emp_no
+ORDER BY emp_no
+
+SELECT
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no
+order by emp_no;
+
+
+SELECT
+	employees.last_name,
+	employees.first_name,
+	employees.sex
+from employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+SELECT 
+	employees.emp_no, 
+	employees.last_name, 
+	employees.first_name
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
+
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development')
+
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
